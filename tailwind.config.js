4b4730ff3654b4e4afbc7ddc0f6785074d752deb
Rakshit Arora <rakshit087@gmail.com>
2021-11-23 00:17:18 +0530

    Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      bkgrnd: '#FEFBF3',
+      frgrnd: '#79B4B7',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
   },
   variants: {
     extend: {},
