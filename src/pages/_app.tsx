fae8a174dc692b8e16a2ffdb9fcafab4ae0f42ce
Rakshit Arora <rakshit087@gmail.com>
2021-11-23 00:30:53 +0530

    Fixed Issues with Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
-
+import '../assets/global.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      bkgrnd: '#FEFBF3',
-      frgrnd: '#79B4B7',
-    },
     extend: {
       fontFamily: {
         poppins: ['Poppins'],
+      },
+      colors: {
+        bkgrnd: '#FEFBF3',
+        frgrnd: '#79B4B7',
       },
     },
   },
