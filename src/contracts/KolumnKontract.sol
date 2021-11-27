125e767b5daba89524655f1c4b51d56992d6343e
Rakshit Arora <rakshit087@gmail.com>
2021-11-28 02:02:17 +0530

    Updated createKonlumn

--- src/contracts/KolumnKontract.sol
+++ src/contracts/KolumnKontract.sol
@@ -6,15 +6,28 @@ contract KolumnKontract {
     struct Kolumn {
         string title;
         string content;
+        uint256 timestamp;
+        address payable author;
     }
 
     uint256 public kolumnKount = 0;
     mapping(uint256 => Kolumn) public kolumns;
 
     //Add a new Kolumn
-    function createKolumn(string memory _title, string memory _content) public {
+    function createKolumn(
+        string memory _title,
+        string memory _content,
+        uint256 _timestamp
+    ) public {
+        require(msg.sender != address(0x0));
+        require(bytes(_title).length * bytes(_content).length > 0);
         kolumnKount++;
-        kolumns[kolumnKount] = Kolumn(_title, _content);
+        kolumns[kolumnKount] = Kolumn(
+            _title,
+            _content,
+            _timestamp,
+            payable(msg.sender)
+        );
     }
 
     //View Kolumn by ID
