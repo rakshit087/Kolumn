151199b77d622c80dcf96009f9a508b7ec7d2ece
Rakshit Arora <rakshit087@gmail.com>
2021-11-28 13:54:39 +0530

    Add init, connect and isConnected to Web3Service

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -1,3 +1,4 @@
+import Router from "next/router";
 import { ethers } from "ethers";
 import KolumnArtifact from "../abis/KolumnKontract.json";
 
@@ -5,27 +6,39 @@ declare let window: any;
 const address = KolumnArtifact.networks[5777].address;
 
 export const Web3Service = {
-  //Check if user is connected to metamask or not
-  isConnected: async () => {
+  //Add Event Listners and check for Metamask
+  init: async () => {
     if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      try {
-        await signer.getAddress();
-        return true;
-      } catch {
-        return false;
-      }
+      window.ethereum.on("accountsChanged", () => {
+        Router.reload();
+      });
+      window.ethereum.on("disconnect", () => {
+        console.log("Disconnected");
+        Router.reload();
+      });
+      window.ethereum.on("connect", () => {
+        console.log("Connected!");
+      });
+      window.ethereum.on("chainChanged", () => {
+        Router.reload();
+      });
     } else {
       throw Error("Metamask Not Found");
     }
   },
   //Connect to Metamask
   connect: async () => {
-    if (typeof window.ethereum !== "undefined") {
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-    } else {
-      throw Error("Metamask Not Found");
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  },
+  //Check if user is connected to metamask or not
+  isConnected: async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    try {
+      await signer.getAddress();
+      return true;
+    } catch {
+      return false;
     }
   },
 };
