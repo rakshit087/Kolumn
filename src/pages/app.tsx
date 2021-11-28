9e7c01a50ab4bdbd23f9ea1b27b1b55409976ead
Rakshit Arora <rakshit087@gmail.com>
2021-11-28 13:51:55 +0530

    Load the Application depending on connection

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,15 +1,34 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 //Importing Layouts
-import NavBar from '../layouts/Navbar';
+import NavBar from "../layouts/Navbar";
+//Importing Web3 Services
+import { Web3Service } from "../services/Web3Service";
 
 const App: NextPage = () => {
+  const router = useRouter();
+  const [connected, setConnected] = useState<Boolean>(true);
+
+  useEffect(() => {
+    Web3Service.isConnected().then((con) => {
+      setConnected(con);
+    });
+  }, []);
+  useEffect(() => {
+    if (connected == false)
+      router.push({
+        pathname: "/",
+      });
+  }, [connected]);
+
   return (
     <div>
       <Head>
         <title>Kolumn</title>
-        <meta name='Kolumn' content='Publish your articles on Blockchain' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="Kolumn" content="Publish your articles on Blockchain" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
       <p>You are connected</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,35 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 //Importing Layouts
-import NavBar from '../layouts/Navbar';
-import FrontBox from '../layouts/FrontBox';
+import NavBar from "../layouts/Navbar";
+import FrontBox from "../layouts/FrontBox";
 //Improting Web3 Services
-import { Web3Service } from '../services/Web3Service';
+import { Web3Service } from "../services/Web3Service";
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const [connected, setConnected] = useState<Boolean | Promise<Boolean>>(false);
-  useEffect(()=>{
-    setConnected(Web3Service.isConnected());
-  },[])
-  if(connected == true) router.push({
-    pathname: '/app'
-  })
+  const [connected, setConnected] = useState<Boolean>(false);
+
+  useEffect(() => {
+    Web3Service.isConnected().then((con) => {
+      setConnected(con);
+    });
+  }, []);
+  useEffect(() => {
+    if (connected == true)
+      router.push({
+        pathname: "/app",
+      });
+  }, [connected]);
+
   return (
     <div>
       <Head>
         <title>Kolumn</title>
-        <meta name='Kolumn' content='Publish your articles on Blockchain' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="Kolumn" content="Publish your articles on Blockchain" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
       <FrontBox />
