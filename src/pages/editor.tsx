22a2f38fa9a1c84283d97c9087870e9a19aed3d5
Rakshit Arora <rakshit087@gmail.com>
2021-12-01 22:58:18 +0530

    Started working on Editor

---
+++ src/pages/editor.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+//Importing Layouts
+import NavBar from "../layouts/Navbar";
+//Importing Web3 Services
+import { Web3Service } from "../services/Web3Service";
+
+const App: NextPage = () => {
+  const router = useRouter();
+  const [connected, setConnected] = useState<Boolean>(true);
+
+  useEffect(() => {
+    Web3Service.isConnected().then((con) => {
+      setConnected(con);
+    });
+  }, []);
+  useEffect(() => {
+    if (connected == false)
+      router.push({
+        pathname: "/",
+      });
+  }, [connected]);
+
+  return (
+    <div>
+      <Head>
+        <title>Kolumn</title>
+        <meta name="Kolumn" content="Publish your articles on Blockchain" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar connected={connected} />
+      <p>You are connected</p>
+    </div>
+  );
+};
+
+export default App;
