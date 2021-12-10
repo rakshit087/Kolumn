3496212c46deef890873c48901631b234c6c6d2d
Rakshit Arora <rakshit087@gmail.com>
2021-12-10 10:54:30 +0530

    Added Ropsten Endpoint

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const INFURA_ENDPOINT = process.env.INFURA_ENDPOINT;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: INFURA_ENDPOINT,
+      accounts: [PRIVATE_KEY],
+    },
+  },
   paths: {
     sources: "./src/contracts",
     tests: "./test",
