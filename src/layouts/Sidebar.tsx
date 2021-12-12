5bc8589abb747a14445f9cbf7ca322c2f6629c34
Rakshit Arora <rakshit087@gmail.com>
2021-12-12 20:02:20 +0530

    Fixed Width Issues

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -2,8 +2,8 @@ import LinkButton from "../components/LinkButton";
 
 const Sidebar = () => {
   return (
-    <div className="hidden md:flex md:flex-col items-end border-l-2 md:w-64 lg:w-96 b-l h-[calc(720px-6rem)]">
-      <div className="bg-bkgrnd w-56 lg:w-72 my-10 py-8 px-4">
+    <div className="hidden md:flex md:flex-col items-end border-l-2 md:w-64 lg:w-72 b-l h-[calc(100vh-5rem)]">
+      <div className="bg-bkgrnd w-56 lg:w-60 my-10 py-8 px-4">
         <p className="font-mono text-sm">Write on Konnect</p>
         <ul className="list-disc px-8 my-5 font-serif">
           <li>Own your Articles</li>
