4cfb311a72f82b388939cfa64b8e81151db579e9
Rakshit Arora <rakshit087@gmail.com>
2021-12-13 22:04:13 +0530

    Increased Z index for Navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const NavBar = (props: Props) => {
   return (
-    <div className="sticky top-0 flex justify-center w-screen h-20 px-4 transition-all border-b border-black bg-bkgrnd md:px-20 lg:px-40">
+    <div className="sticky top-0 z-10 flex justify-center w-screen h-20 px-4 transition-all border-b border-black bg-bkgrnd md:px-20 lg:px-40">
       <div className="flex items-center justify-between w-full h-full max-w-6xl">
         <Link href={props.connected ? "/main" : "/"} passHref>
           <p className="text-4xl cursor-pointer font-poppins">Kolumn</p>
