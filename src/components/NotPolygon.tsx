1d896cceadc3505200b723120ab6dacf0bd83d00
Rakshit Arora <rakshit087@gmail.com>
2021-12-15 02:06:31 +0530

    Checks if user is connected to Polygon Testnet or not

---
+++ src/components/NotPolygon.tsx
@@ -0,0 +1,40 @@
+import ActionButton from "./ActionButton";
+
+declare let window: any;
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen px-4 text-center bg-bkgrnd">
+      <p className="text-3xl">You are not on Polygon TestNet.</p>
+      <p className="text-3xl">
+        If it is not added to your wallet you can add it from button below 👇
+      </p>
+
+      <div className="flex items-center justify-center mt-10 cursor-pointer bg-frgrnd text-bkgrnd w-60 h-14">
+        <ActionButton
+          text="Add Polygon Testnet"
+          clickHandler={() => {
+            window.ethereum.request({
+              method: "wallet_addEthereumChain",
+              params: [
+                {
+                  chainName: "Polygon Testnet Mumbai",
+                  chainId: "0x13881",
+                  nativeCurrency: {
+                    name: "MATIC",
+                    symbol: "MATIC",
+                    decimals: 18,
+                  },
+                  rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
+                  blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+                },
+              ],
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,21 @@ import "tailwindcss/tailwind.css";
 import "../assets/global.css";
 import Loading from "../components/Loading";
 import NotFound from "../components/NotFound";
+import NotPolygon from "../components/NotPolygon";
 
 declare let window: any;
 
 function MyApp({ Component }: AppProps) {
   const [meta, setMeta] = useState<Boolean | undefined>(undefined);
+  const [poly, setPoly] = useState<Boolean | undefined>(undefined);
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
+      window.ethereum
+        .request({ method: "eth_chainId" })
+        .then((chainID: number) => {
+          if (chainID == 80001) setPoly(true);
+          else setPoly(false);
+        });
       window.ethereum.on("accountsChanged", () => {
         window.location.reload();
       });
@@ -22,13 +30,12 @@ function MyApp({ Component }: AppProps) {
       setMeta(false);
     }
   }, []);
-  return meta == undefined ? (
-    <Loading />
-  ) : meta == true ? (
-    <Component />
-  ) : (
-    <NotFound />
-  );
+  if (meta == undefined) return <Loading />;
+  else if (meta) {
+    if (poly == undefined) return <Loading />;
+    if (poly) return <Component />;
+    else return <NotPolygon />;
+  } else return <NotFound />;
 }
 
 export default MyApp;
