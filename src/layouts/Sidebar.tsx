1e9f9dfe15e6144dcfaa6f38d70004f02c538949
Rakshit Arora <50898928+rakshit087@users.noreply.github.com>
2021-12-15 23:26:51 +0530

    Update Sidebar.tsx

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -4,9 +4,9 @@ const Sidebar = () => {
   return (
     <div className="sticky top-20 hidden md:flex md:flex-col items-end border-l-2 md:w-64 lg:w-72 b-l h-[calc(100vh-5rem)]">
       <div className="w-56 px-4 py-8 my-10 bg-bkgrnd lg:w-60">
-        <p className="font-mono text-sm">Write on Konnect</p>
+        <p className="font-mono text-sm">Write on Kolumn</p>
         <ul className="px-8 my-5 font-serif list-disc">
-          <li>Own your Stories</li>
+          <li>Own your Work</li>
           <li>Earn Crypto!</li>
         </ul>
         <div className="px-4">
